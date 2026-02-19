cd5b3ec9601224fd083cc900a3c8e4c4f00a6f92
RattK <37528531+krirat@users.noreply.github.com>
2026-02-19 21:01:32 +0700

    Merge with o/main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import RecipeOfTheDay from "./component/RecipeOfTheDay";
-=======
-import logo from './logo.svg';
->>>>>>> 50b5ba4f87cf005acc427191a26a28c941625125
 
 function App() {
   return <RecipeOfTheDay />;
