1008dcd871cb0801bdb2f1bc72fe4b65169070a0
RattK <37528531+krirat@users.noreply.github.com>
2026-02-24 17:47:59 +0700

    two new routes: /recipes/:id and 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router';
 import { BrowserRouter } from "react-router";
 import MainPage from './mainPage'; // Change checkboxes
+import RecipePage from './recipePage'; // Recipe Page
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/recipes/:id" element={<RecipePage />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
