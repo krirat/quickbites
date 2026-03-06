9675d47726eb659cd12d046cbf55091e18d95569
RattK <37528531+krirat@users.noreply.github.com>
2026-03-06 16:41:52 +0700

    improved search bar appearance

--- frontend/src/components/searchBar.js
+++ frontend/src/components/searchBar.js
@@ -4,7 +4,11 @@ import useDebounce from './useDebounce';
 
 const data = [
     { id: 1, title: 'Spaghetti Carbonara' },
-    { id: 2, title: 'Chicken Alfredo' },]
+    { id: 2, title: 'Chicken Alfredo' },
+    { id: 3, title: 'Chicken Noodle Soup' },
+    { id: 4, title: 'Pad Krapow' },
+]
+
 
 /**
 @param {string} data_source - The URL of the data source to fetch data from
@@ -13,6 +17,7 @@ const data = [
 function SearchBar({ data_source }) {
     const [search, setSearch] = useState('');
     const [filteredTitle, setFilteredTitle] = useState([]);
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
 
     //start searching after user stops typing for 800ms
     useDebounce(() => {
@@ -25,21 +30,37 @@ function SearchBar({ data_source }) {
     const handleSearch = (e) => setSearch(e.target.value);
 
     return (
-        <>
+        <div className="flex border-black border-2 max-w-screen-sm">
             <input
                 id="search"
+                className="w-full p-2 focus:outline-none"
                 type="text"
                 spellCheck="false"
                 placeholder="Find a recipe"
                 value={search || ''}
                 onChange={handleSearch}
             />
-            <div>
-                {filteredTitle.map((f) => (
-                    <p key={f.id}>{f.title}</p>
-                ))}
+            <div className="relative h-full">
+                <div className="flex ">
+                    <button
+                        className="border-black border-l-2 px-5 bg-blue-400 hover:bg-blue-500 h-full"
+                        onClick={() => setIsFilterOpen(!isFilterOpen)}
+                    >
+                        Filter
+                    </button>
+                </div>
+                {isFilterOpen && (
+                    <ul className="absolute top-full left-0 w-auto border-black border-2 bg-white">
+                        {filteredTitle.map((item) => (
+                            <li key={item.id} className="p-2 hover:bg-gray-200">
+                                {item.title}
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
-        </>
+            <button className="border-black border-l-2 px-5 bg-blue-400 hover:bg-blue-500">Search</button>
+        </div>
     );
 }
 
