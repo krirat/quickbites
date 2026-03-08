4623bd0a53f0e0ec647cdbaa9e4beb917e2f07b0
RattK <37528531+krirat@users.noreply.github.com>
2026-03-08 18:34:59 +0700

    recipe page fetches data

--- frontend/src/recipePage.js
+++ frontend/src/recipePage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams, useNavigate } from 'react-router'; 
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router';
 import IntroSection from "./components/introSection";
 import RecipeDetail from "./components/RecipeDetail";
 import RecipeHowto from "./components/RecipeHowto";
@@ -7,20 +7,50 @@ import RecipeHowto from "./components/RecipeHowto";
 function RecipePage() {
     const { id } = useParams();
     const navigate = useNavigate(); // 2. Initialize the hook
+    const [data, setData] = useState({})
+    const [loading, setLoading] = useState(true);
+    console.log("RecipePage received recipeId:", id);
+
+    useEffect(() => {
+        console.log("hi")
+        const fetchData = async () => {
+            try {
+                //setSteps(MOCK_STEPS);
+                console.log("Fetching recipe data for ID:", id);
+                const response = await fetch(`http://localhost:5003/api/recipe/${id}`);
+                if (!response.ok) throw new Error("Failed to fetch recipe data");
+                const data = await response.json();
+                setData(data);
+                console.log("Fetched recipe data:", data);
+            } catch (err) {
+                console.log(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [id]);
 
     return (
         <div className="recipe-page-container relative">
             {/* 3. The Back Button */}
-            <button 
-                onClick={() => navigate('/')} 
+            <button
+                onClick={() => navigate('/')}
                 className="absolute top-4 left-4 z-10 bg-black text-white px-4 py-2 rounded-full hover:bg-gray-800 transition-colors shadow-lg"
             >
                 ← Back to Menu
             </button>
 
-            <IntroSection recipeId={id} />
-            <RecipeDetail recipeId={id} />
-            <RecipeHowto   recipeId={id}/>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <IntroSection recipeId={id} recipeData={data} />
+                    <RecipeDetail recipeId={id} recipeData={data} />
+                    <RecipeHowto recipeId={id} />
+                </>
+            )}
         </div>
     );
 }
